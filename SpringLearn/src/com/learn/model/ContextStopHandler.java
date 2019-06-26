533bf9d6c52b30f00cd6a4eaa0c0733aa63e5975
webdeveloper <webdeveloper.ygt@gmail.com>
2019-06-26 13:41:37 +0530

    checking PR

--- SpringLearn/src/com/learn/model/ContextStopHandler.java
+++ SpringLearn/src/com/learn/model/ContextStopHandler.java
@@ -2,7 +2,6 @@ package com.learn.model;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextStoppedEvent;
-
 //@Spring
 public class ContextStopHandler implements ApplicationListener<ContextStoppedEvent>{
 
